6e1afb88410ea6bb1b862a66369f1051a57883c5
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-07 23:31:36 +0530

    single player

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         System.out.println("My first game, Snake and Ladder");
+        System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6 \n" +
+                "A safe place, ladder or a snake \n" + "keep doing till the winning spot 100 is achieved.\n\n");
+
+
+        int player1Position = 0;
+        System.out.println("Single player position: "+ player1Position);
+
     }
 }
