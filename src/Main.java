b1c57d161c0c2d32bf80768dcd8dd8333b3ea8aa
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-07 22:51:01 +0530

    My first game

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My first game");
+        System.out.println("My first game, Snake and Ladder");
     }
 }
