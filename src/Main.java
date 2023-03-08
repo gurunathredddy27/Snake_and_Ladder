53c909627e17ba169fa1c5e8aef218f11480a21c
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-08 12:45:59 +0530

    reaches position 100, while loop

--- src/Main.java
+++ src/Main.java
@@ -3,26 +3,33 @@ public class Main {
     public static void main(String[] args) {
 
         System.out.println("This problem simulates a Snake and Ladder Game. \n The Player starts from 0 rolls the die to get a number between 1 to 6, \n" +
-                "finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved. \n\n");
+                "finds a safe place ladder or a snake keeps doing till the winning spot 100 is achieved. \n\n");
 
         Random random = new Random();
+
         int player1Position = 0;
-        System.out.println("Player start position: "+ player1Position);
+        System.out.println("Player start position: " + player1Position);
+
+        while (player1Position <= 100) {
+            int rollDice = random.nextInt(6) + 1;
+            System.out.println("the player got number " + rollDice);
+            int option = random.nextInt(3); // 0,1, 3
+            System.out.println("The option you got is " + option);
 
 
-        int rollDice = random.nextInt(6)+ 1;
-        int option = random.nextInt(3); // 0,1, 3
-        System.out.println("The number you got is "+ rollDice );
-
-
-        if (option == 0){
-            System.out.println("no play, you have to stay in same position "+ player1Position);
-        }else if (option == 1){
-            player1Position += rollDice;
-            System.out.println("You got ladder, moves ahead of position "+ player1Position);
-        } else {
-        player1Position -= rollDice;
-        System.out.println("You got Snake, moves behind of position "+ player1Position);
+            if (option == 0) {
+                System.out.println("no play, you have to stay in same position " + player1Position);
+            } else if (option == 1) {
+                player1Position += rollDice;
+                System.out.println("You got ladder, moves ahead of position " + player1Position);
+            } else {
+                player1Position -= rollDice;
+                System.out.println("You got Snake, moves behind of position " + player1Position);
+            }
+            if (player1Position < 0){
+                System.out.println("the player start from Zero "+ player1Position);
+            }
         }
+        System.out.println("the player reaches winning position "+ player1Position);
     }
 }
