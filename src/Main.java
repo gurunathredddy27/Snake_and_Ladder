7b13e7a1943db5380fde91254c50ee8a140ad848
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-08 12:01:22 +0530

    how to play

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("This problem simulates a Snake and Ladder Game. \n The Player starts from 0 rolls the die to get a number between 1 to 6, \n" +
+                "finds a safe place, ladder or a snake keeps doing till the winning spot 100 is achieved. \n\n");
 
         Random random = new Random();
         int player1Position = 0;
