217858202e644515b64f850273c628885d66246e
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-08 22:07:48 +0530

    win position

--- src/Main.java
+++ src/Main.java
@@ -10,18 +10,20 @@ public class Main {
         int player1Position = 0;
         System.out.println("Player start position: " + player1Position);
 
-        while (player1Position <= 100) {
+        while (player1Position < 100) {
             int rollDice = random.nextInt(6) + 1;
             System.out.println("the player got number " + rollDice);
-            int option = random.nextInt(3); // 0,1, 3
+            int option = random.nextInt(3); // 0,1, 2
             System.out.println("The option you got is " + option);
 
 
             if (option == 0) {
                 System.out.println("no play, you have to stay in same position " + player1Position);
             } else if (option == 1) {
+
                 player1Position += rollDice;
                 System.out.println("You got ladder, moves ahead of position " + player1Position);
+
             } else {
                 player1Position -= rollDice;
                 System.out.println("You got Snake, moves behind of position " + player1Position);
