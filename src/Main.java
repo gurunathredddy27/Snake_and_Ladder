4d0b252014ac97d6f243ac9efe0ed135b768e74d
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-08 11:17:56 +0530

    roll the number between 1 to 6

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("My first game, Snake and Ladder");
         System.out.println("The Player starts from 0 rolls the die to get a number between 1 to 6 \n" +
-                "A safe place, ladder or a snake \n" + "keep doing till the winning spot 100 is achieved.\n\n");
+                "A safe place, ladder or danger place the snake \n" + "keep doing till the winning spot 100 is achieved.\n\n");
 
 
         int player1Position = 0;
         System.out.println("Single player position: "+ player1Position);
+
+        Random random = new Random();
+        int rollDice = random.nextInt(6)+ 1;
+        System.out.println("The number you got is "+ rollDice);
+
+
 
     }
 }
