7cabc81931d40708aa9bb7b8f57ea237e8a156ca
gurunathreddy2727@gmail.com <gurunathreddy2727@gmail.com>
2023-03-08 22:36:45 +0530

    exact position ie 100

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Random;
-public class Main {
+class Main {
     public static void main(String[] args) {
 
         System.out.println("This problem simulates a Snake and Ladder Game. \n The Player starts from 0 rolls the die to get a number between 1 to 6, \n" +
@@ -10,7 +10,7 @@ public class Main {
         int player1Position = 0;
         System.out.println("Player start position: " + player1Position);
 
-        while (player1Position <= 100) {
+        while (player1Position < 100) {
             int rollDice = random.nextInt(6) + 1;
             System.out.println("the player got number " + rollDice);
             int option = random.nextInt(3); // 0,1, 3
@@ -21,15 +21,22 @@ public class Main {
                 System.out.println("no play, you have to stay in same position " + player1Position);
             } else if (option == 1) {
                 player1Position += rollDice;
+                if (player1Position > 100) {
+                    player1Position -= rollDice;
+                }
                 System.out.println("You got ladder, moves ahead of position " + player1Position);
             } else {
                 player1Position -= rollDice;
-                System.out.println("You got Snake, moves behind of position " + player1Position);
+                if (player1Position < 0) {
+                    player1Position = 0;
+                }
+                System.out.println("SYou got Snake, moves behind of position  " + player1Position);
             }
-            if (player1Position < 0){
-                System.out.println("the player start from Zero "+ player1Position);
-            }
+
         }
-        System.out.println("the player reaches winning position "+ player1Position);
+        System.out.println("hurray!! you have reached final winning position 100");
+
     }
 }
+
+
